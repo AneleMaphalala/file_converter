228aab251c2141dffdfa406d9d9bcfda91d8fbd3
Anele Maphalala <maphalalaanele@gmail.com>
2023-11-11 02:46:59 +0200

    Updated the conversion from pdf to word document

--- document_format_converter/format_converter.py
+++ document_format_converter/format_converter.py
@@ -4,11 +4,27 @@ def pdf_to_docx():
     cv = Converter(pdf_file)
     cv.convert(docx_file)
     cv.close()
+
+def docx_to_pdf():
+    '''
+    Add code to convert 
+    document file to a pdf file
+    '''
  
 
-
 if __name__ == '__main__':
-    pdf_file = input('Enter pdf file name: ')
-    docx_file = input('Enter the name of the new document file: ')
-    pdf_to_docx()
-    print('Successfully converted from pdf into document file!')
+    print("The types of conversions" + "\n1. PDF to DOCUMENT" + "\n2. DOCUMENT to PDF")
+    type_of_conversion = input("\nEnter the number of the conversion you want: ")
+
+    if type_of_conversion == "1":
+        pdf_file = input('Enter pdf file name: ')
+        docx_file = input('Enter the name of the new document file: ')
+        pdf_to_docx()
+        print('Successfully converted from pdf into document file!')
+
+    elif type_of_conversion == "2":
+        docx_file = input("Enter the document file name: ")
+        pdf_file = input("Enter the name of the new document: ")
+        docx_to_pdf()
+        print('Succesffuly converted from docx to pdf file!')
+ 
