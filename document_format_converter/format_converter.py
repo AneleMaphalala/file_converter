940d2c0a38483a8b41a75e26eb98e6f083bf425a
Anele Maphalala <maphalalaanele@gmail.com>
2023-11-19 16:09:14 +0200

    Updated conversion from document to pdf

--- document_format_converter/format_converter.py
+++ document_format_converter/format_converter.py
@@ -1,4 +1,5 @@
 from pdf2docx import Converter
+from docx2pdf import convert
 
 def pdf_to_docx():
     cv = Converter(pdf_file)
@@ -6,25 +7,28 @@ def pdf_to_docx():
     cv.close()
 
 def docx_to_pdf():
-    '''
-    Add code to convert 
-    document file to a pdf file
-    '''
- 
+    convert(docx_file, pdf_file) 
 
 if __name__ == '__main__':
-    print("The types of conversions" + "\n1. PDF to DOCUMENT" + "\n2. DOCUMENT to PDF")
-    type_of_conversion = input("\nEnter the number of the conversion you want: ")
+    while True:
+        print("The types of conversions\n1. PDF to DOCUMENT\n2. DOCUMENT to PDF\n3. Exit\n")
+        type_of_conversion = input("\nEnter the number of the conversion you want: ")
 
-    if type_of_conversion == "1":
-        pdf_file = input('Enter pdf file name: ')
-        docx_file = input('Enter the name of the new document file: ')
-        pdf_to_docx()
-        print('Successfully converted from pdf into document file!')
+        if type_of_conversion == "1":
+            pdf_file = input('Enter pdf file name: ')
+            docx_file = input('Enter the name of the new document file: ')
+            pdf_to_docx()
+            print('Successfully converted from pdf into document file!')
 
-    elif type_of_conversion == "2":
-        docx_file = input("Enter the document file name: ")
-        pdf_file = input("Enter the name of the new document: ")
-        docx_to_pdf()
-        print('Succesffuly converted from docx to pdf file!')
- 
+        elif type_of_conversion == "2":
+            docx_file = input("Enter the document file name: ")
+            pdf_file = input("Enter the name of the new document: ")
+            docx_to_pdf()
+            print('Successfuly converted from document to pdf file!')
+
+        elif type_of_conversion == "3":
+            print('Exiting the program.\nGoodbye!')
+            break
+
+        else:
+            print("Invalid input.\nPlease enter a valid option.")
